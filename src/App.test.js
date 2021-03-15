5221867553e7d5fe2dd4468926f51df516dcd6a9
Fogler Tibor (Utopszkij) <tibor.fogler@gmail.com>
2021-03-15 15:53:18 +0100

    unittest

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/Start editing/i);
+  const linkElement = screen.getByText(/start/i);
   expect(linkElement).toBeInTheDocument();
 });
